121a6248b9f6348b9c88bd8b1a1d5f068d65e12d
Emiliano Fagundes <emiliano.fagundes@hotmail.com>
2018-05-28 13:59:49 -0300

    Estrutura do projeto criado

--- com/models/Projeto.java
+++ com/models/Projeto.java
@@ -1,11 +1,58 @@
 package models;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * 
  * @author Fagundes, Emiliano
- * @since		05-2018
+ * @since 05-2018
  *
  */
-public class Projeto {
 
+public class Projeto {
+	public class Tarefa {
+		String nome;
+		List<Relacao> adj;
+
+		Tarefa(String nome) {
+			this.nome = nome;
+			this.adj = new ArrayList<Relacao>();
+		}
+
+		void addAdj(Relacao e) {
+			adj.add(e);
+		}
+	}
+
+	public class Relacao {
+		Tarefa origem;
+		Tarefa destino;
+
+		Relacao(Tarefa origem, Tarefa destino) {
+			this.origem = origem;
+			this.destino = destino;
+		}
+	}
+
+	List<Tarefa> tarefas;
+	List<Relacao> relacoes;
+
+	public Projeto() {
+		tarefas = new ArrayList<Tarefa>();
+		relacoes = new ArrayList<Relacao>();
+	}
+
+	public String toString() {
+		String r = "";
+		for (Tarefa u : tarefas) {
+			r += u.nome + " -> ";
+			for (Relacao e : u.adj) {
+				Tarefa v = e.destino;
+				r += v.nome + ", ";
+			}
+			r += "\n";
+		}
+		return r;
+	}
 }
